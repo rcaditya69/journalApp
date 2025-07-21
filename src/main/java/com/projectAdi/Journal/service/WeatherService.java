7ad19380c6176c013e63bb3f5a556b5ab0d1f6d1
rcaditya69 <rcaditya69@gmail.com>
2025-07-21 20:54:46 +0530

    adding value to properties

--- src/main/java/com/projectAdi/Journal/service/WeatherService.java
+++ src/main/java/com/projectAdi/Journal/service/WeatherService.java
@@ -2,13 +2,16 @@ package com.projectAdi.Journal.service;
 
 import com.projectAdi.Journal.entity.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-@Component
+@Service
 public class WeatherService {
-    private static final String apikey ="7630aef25d0e3a5245bd2c26819c46a9";
+    @Value("${api.key}")
+    private  String apikey;
     public static final String api="http://api.weatherstack.com/current?access_key=APIKEY&query=CITY";
     @Autowired
     RestTemplate restTemplate;
